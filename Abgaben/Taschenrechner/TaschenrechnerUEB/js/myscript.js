cbcf90206b2717dc0138359c61fd965c8b1e31e4
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-03-10 15:07:53 +0100

    feddisch

---
+++ Abgaben/Taschenrechner/TaschenrechnerUEB/js/myscript.js
@@ -0,0 +1,46 @@
+var a, b, result = 0, logline = "";
+
+function calculate(operator){
+    a = document.getElementById("zahl1").value;
+    b = document.getElementById("zahl2").value;
+    switch(operator){
+        case "+":
+            result = add();
+            logline = a + " + " + b + " = " + result;
+            break;
+        case "-":
+            result = subtract();
+            logline = a + " - " + b + " = " + result;
+            break;
+        case "*":
+            result = multiply();
+            logline = a + " * " + b + " = " + result;
+            break;
+        case "/":
+            result = divide();
+            logline = a + " / " + b + " = " + result;
+            break;
+    }        
+    document.getElementById("ergebnis").innerHTML = result;
+    //alert("calculate: " + logline + "is: " + a + " " + b + " " + result);
+    writeLog(logline); 
+}
+
+function add(){    
+    return (Number(a) + Number(b));
+}
+function subtract(){
+    return (Number(a) - Number(b));
+}
+function multiply(){
+    return (Number(a) * Number(b));
+}
+function divide(){
+    return (Number(a) / Number(b));
+}
+
+function writeLog(logline){
+    //alert("writeLog" + logline);
+    const logs = "<li>" + logline + "</li>" + document.getElementById("verlauf").innerHTML;
+    document.getElementById("verlauf").innerHTML = logs;
+}
