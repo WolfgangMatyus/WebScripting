494078b7ddab6affa10a26cf87af82eea286c14d
WolfgangMatyus <wi21b055@technikum-wien.at>
2023-06-18 23:37:01 +0200

    getCartProducts

--- CodeSnippets/DragAndDrop/script.js
+++ CodeSnippets/DragAndDrop/script.js
@@ -1,37 +1,17 @@
-// Get the draggable elements
-const cards = document.querySelectorAll('.draggable');
-
-// Add event listeners for dragstart and dragend
-cards.forEach(card => {
-  card.addEventListener('dragstart', dragStart);
-  card.addEventListener('dragend', dragEnd);
-});
-
-// Add event listeners for dragover and drop
-const list = document.getElementById('list');
-list.addEventListener('dragover', dragOver);
-list.addEventListener('drop', drop);
-
-// Drag start event handler
-function dragStart(event) {
-  event.dataTransfer.setData('text/plain', event.target.id);
-  event.target.classList.add('dragging');
-}
-
-// Drag end event handler
-function dragEnd(event) {
-  event.target.classList.remove('dragging');
-}
-
-// Drag over event handler
-function dragOver(event) {
-  event.preventDefault();
-}
-
-// Drop event handler
-function drop(event) {
-  event.preventDefault();
-  const cardId = event.dataTransfer.getData('text/plain');
-  const card = document.getElementById(cardId);
-  list.appendChild(card);
-}
+$(function() {
+    $("#draggable").draggable({
+      revert: "invalid",
+      cursor: "move"
+    });
+    
+    $("#droppable").droppable({
+        accept: ".draggable",
+        drop: function(event, ui) {
+          const cardContent = ui.draggable.text();
+          const listItem = $("<li>").text(cardContent);
+          $(this).append(listItem);
+          $(this).addClass("ui-state-highlight");
+          ui.draggable.remove();
+        }
+      });
+  });
--- Webprojekt/resources/public/js/home.js
+++ Webprojekt/resources/public/js/home.js
@@ -9,6 +9,7 @@ $(document).ready(function(){
         success: function(json){
             console.log(json)
             loadProducts(json);
+            loadHomeCart();
         },
         error: function(json){
             console.error("An ERROR occured!")
@@ -18,7 +19,7 @@ $(document).ready(function(){
 
 function loadProducts(data) {
 
-    let productCards = '<div id="productList"></ul>'
+    let productCards = '<ul id="productList"></ul>'
 
     $("#productListContainer")
         .empty()
@@ -41,7 +42,7 @@ function loadProducts(data) {
         
         
         $("#productList").append(
-            '<div class="card" id="card'+ i +'">'
+            '<div class="card" id="card'+ i +'" draggable="true" ondragstart="drag(event)">'
             + '<div class="row g-0">'
             + cardImg
             + cardBody
@@ -61,9 +62,113 @@ function loadProducts(data) {
     });  
 }
 
+//-- Searchfilter -- //
+
+
+
 //-- HOMESHOPPING_CARD --//  
+function loadHomeCart() {
+    let homeCartList = '<ul class="homeCartList" id="homeCartList"></ul>'
+    let homeCartSum = '<div class="rowsum"><div class="col" id="homeCartSum">SUMME:' 
+                    + '</div><div class="col" id="SumValue">0,00<div>'
+
+    $("#homeCart")
+        .append(homeCartList)
+
+    $("#homeCart")
+    .append(homeCartSum)
+
+};
+
+//-- DragAndDrop --//
+function allowDrop(event){
+    event.preventDefault();
+}
+
+function drag(event){
+    event.dataTransfer.setData("text", event.target.id)
+}
+
+function drop(event){
+    event.preventDefault();
+    var data = event.dataTransfer.getData("text");
+    var draggedElement = document.querySelector("#0"); // "#" + data
+
+    sendCartProduct(data);
+}
+//-- write ShoppingCart to CartProductsJson
+function sendCartProduct(data){
+    
+    var cartproductid = {
+        id: data,
+    };
+    
+    $.ajax({
+        method: "POST",
+        url: "/api/v1/auth/cart",
+        contentType: "application/json",
+        data: JSON.stringify(data),
+
+        success: function(response){
+            //console.log(response)
+            loadCart(response);
+        },
+        error: function(response){
+            console.error("An ERROR occured!")
+        }
+    })
+}
 
 
+function loadCart(response){
+
+
+    //WORKAROUND LOAD TEST cartproducts.json
+    $.ajax({
+        method: "GET",
+        dataType: "json",
+        url: "../../Data/cartproducts.json",
+
+        success: function(json){
+            console.log(json)
+            
+        },
+        error: function(json){
+            console.error("An ERROR occured!")
+        }
+    })
+
+    // END WORKAROUND LOAD TEST cartproducts.json
+
+    $.each(data, function (i, data) {
+    // Create a new list item element
+    var listItem = document.createElement("li");
+    listItem.id = data;
+    
+    let cardBody = '<div class="row">'
+    + '<div class="card-body col" id="' + data.id + '">'
+    + '<p class="productName col">' + data.name + '</p>'
+    + '<p class="productPrice col">Price: ' + data.quantity + '</p>'
+    + '<p class="productPrice col">Price: ' + data.price_single + '</p>'
+    + '<p class="productPrice col">Price: ' + data.price_total + '</p>'
+    + '</div>'
+    + '</div>'
+
+    
+    listItem.textContent = cardBody;
+
+    
+
+    // Append the new list item to the target element
+    $("#homeCartList").append(listItem);
+    })
+}
+    
+//-- HomeCardSum-Calculator --/
+function calculateHomeCardSum(){
+    
+
+}
 
  //console.log("do kumma hi");
 
